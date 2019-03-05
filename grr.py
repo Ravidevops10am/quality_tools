240a73080e33d0ef5d6eeb117a69a1881a646e5a
joe tats <tatuskojc@gmail.com>
2019-03-04 23:43:42 -0500

    formatting update

--- grr.py
+++ grr.py
@@ -1,8 +1,9 @@
 import numpy as np 
 
 def grr(measurements, parts, appraisers, limit_range=None):
-    """ Calculate Gage R&R statistics based on a set of measurements
-        with their respective part and apprasier identifiers, optionally
-        include limit tolerances. """
+    """Calculate Gage R&R statistics based on a set of measurements
+    with their respective part and apprasier identifiers, optionally
+    include limit tolerances. 
+    """
 
     pass
