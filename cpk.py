0eea877d1552e055579b885d07c2408c3a3c309c
joe tats <tatuskojc@gmail.com>
2019-03-04 23:44:09 -0500

    update formatting

--- cpk.py
+++ cpk.py
@@ -2,15 +2,16 @@ import numpy as np
 
 
 def cpk(measurements, lower_limit, upper_limit=None):
-    """ Calculate Cpk statistics for a given set of measurements
-        with one or two sided measurements. """
+    """Calculate Cpk statistics for a given set of measurements
+    with one or two sided measurements.
+    """
 
     result = {
         'mean': np.mean(measurements),
         'std' : np.std(measurements, ddof=1)
     }
 
-    if upper_limit:
+    if upper_limit not None:
         result['lower_limit'] = lower_limit
         result['upper_limit'] = upper_limit
         result['cpl'] = (result['mean'] - result['lower_limit']) / (3 * result['std'])
